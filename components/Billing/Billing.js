e56b309d865d1617e4e919ba2f2d1e24e8169b8b
the-jack-white <46023638+the-jack-white@users.noreply.github.com>
2022-10-27 00:07:51 +0100

    Add Billing Component

--- components/Billing/Billing.js
+++ components/Billing/Billing.js
@@ -1,7 +1,46 @@
-import React from "react";
+import Image from "next/image";
+import { apple, bill, google } from "../../assets";
+import styles, { layout } from "../styles/style";
 
 const Billing = () => {
-  return <div>Billing</div>;
+  return (
+    <section id="product" className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}>
+        <Image
+          src={bill}
+          alt="billing"
+          className="w-[100%] h-[100%] relative z-[5]"
+        />
+
+        <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+        <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
+      </div>
+
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Easily control your <br className="sm:block hidden" />
+          billing & invoicing
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic itaque
+          ipsa, culpa molestiae suscipit fugiat vero delectus, ad mollitia ex
+          rem illo nesciunt omnis ducimus dolorum atque saepe quos cum?
+        </p>
+        <div className="flex flex-row flex-wrap gap-4 sm:mt-10 mt-6">
+          <Image
+            src={apple}
+            alt="apple_store"
+            className="w-[128px] h-[42px] object-contain cursor-pointer"
+          />
+          <Image
+            src={google}
+            alt="google_play"
+            className="w-[128px] h-[42px] object-contain cursor-pointer"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Billing;
