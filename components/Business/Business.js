d49dca39a30377f1b9be6c8c8d09efd616a95e56
the-jack-white <46023638+the-jack-white@users.noreply.github.com>
2022-10-27 00:17:35 +0100

    Add Card Deal component

--- components/Business/Business.js
+++ components/Business/Business.js
@@ -44,7 +44,7 @@ const Business = () => {
           building credit, earning rewards and saving money. But with hundreds
           of credit cards on the market.
         </p>
-        <Button styles="mt-10 rounded-[14px]" />
+        <Button styles="mt-10" />
       </div>
 
       <div className={`${layout.sectionImg} flex-col`}>
--- components/CardDeal/CardDeal.js
+++ components/CardDeal/CardDeal.js
@@ -1,7 +1,30 @@
-import React from "react";
+import { card } from "../../assets";
+import styles, { layout } from "../styles/style";
+import Button from "../Button/Button";
+import Image from "next/image";
 
 const CardDeal = () => {
-  return <div>CardDeal</div>;
+  return (
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Find a better card deal <br className="sm:block hidden" />
+          in a few easy steps
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum
+          culpa quibusdam exercitationem cupiditate repellendus unde? Itaque
+          consequatur, minus culpa ex animi debitis similique saepe, tempore
+          dolorum magni velit provident fugiat!
+        </p>
+        <Button styles="mt-10" />
+      </div>
+
+      <div className={layout.sectionImg}>
+        <Image src={card} alt="card" className="w-[100%] h-[100%]" />
+      </div>
+    </section>
+  );
 };
 
 export default CardDeal;
