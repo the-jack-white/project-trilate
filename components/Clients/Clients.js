dd13779022818ff9df67cf94fc498810c9716cfc
the-jack-white <46023638+the-jack-white@users.noreply.github.com>
2022-10-27 01:29:05 +0100

    Add Clients [Styling Broken]

--- components/Clients/Clients.js
+++ components/Clients/Clients.js
@@ -1,7 +1,27 @@
-import React from "react";
+import Image from "next/image";
+import { clients } from "../../constants";
+import styles from "../styles/style";
 
 const Clients = () => {
-  return <div>Clients</div>;
+  return (
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((client) => (
+          <div
+            key={client.id}
+            className={`${styles.flexCenter} flex-1 xs:min-w-[192px] min-w-[100px]`}
+            // className={`${styles.flexCenter} flex-1 sm:min-w-[192px] min-w-[100px]`}
+          >
+            <Image
+              src={client.logo}
+              alt="client"
+              className="xs:w-[192px] w-[100px] object-contain"
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Clients;
